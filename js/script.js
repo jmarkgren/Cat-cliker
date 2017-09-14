5854e3e1a2d83048bca061cd293b495110695135
Janelle Markgren <jsmarkgren@gmail.com>
2017-09-13 21:58:47 -0300

    create cat object

--- js/script.js
+++ js/script.js
@@ -1,47 +1,70 @@
-// var buttonOne = document.getElementById('cat-1');
-// var counterOne = document.getElementById('score-text-1'),
-//   count = 0;
-// buttonOne.onclick = function() {
-//   count += 1;
-//   counterOne.innerHTML = "Cat 1 clicks: " + count;
-// };
+// Create HTML cat name element
+var HTMLcatName = '<p class="cat-name">%data%</p>';
 
-// var buttonTwo = document.getElementById('cat-2');
-// var counterTwo = document.getElementById('score-text-2'),
-//   count = 0;
-// buttonTwo.onclick = function() {
-//   count += 1;
-//   counterTwo.innerHTML = "Cat 2 clicks: " + count;
-// };
+// Set the names of cat one and cat two
+var catOneName = HTMLcatName.replace("%data%", "Kelly");
+//var catTwoName = HTMLcatName.replace("%data%", "John");
 
-var cat = {
-  one:"Julie",
-  two:"Ken",
-  };
+// Show the cat names on the page
+$("#cat-1").prepend(catOneName);
+//$("#cat-2").prepend(catTwoName);
 
-// Create cats
+// Create clicker and counter for cat 1
+var button = document.getElementById('cat-pic-1');
+var counter = document.getElementById('score-text'),
+count = 0;
 
-var cats = {
-  "cat-1": [{
-    "name": "Julie",
-    "picture": "img/cat.jpg",
-    "clicks": "0",
-  }]
+button.onclick = function() {
+  count += 1;
+  counter.innerHTML = "Clicks: " + count;
+};
 
-  "cat-2": [{
-    "name" : "Ken",
-    "picture": "img/cat2.jpg",
-    "clicks": "0",
-  }]
+
+// Create cats... this was confusing, try again later
+// var cats = {
+//   "catOne": [{
+//     "name": "Julie",
+//     "picture": "img/cat.jpg",
+//     "clicks": "0",
+//   }],
+
+//   "catTwo": [{
+//     "name" : "Ken",
+//     "picture": "img/cat2.jpg",
+//     "clicks": "0",
+//   }]
+// }
+
+// Create Cat two object
+var catTwo = {
+  "name": "Ken",
+  "photo": "img/cat2.jpg",
+  "counter": "Clicks: 0"
 }
 
-document.getElementById('cat-1-name').innerHTML = cat.one;
-document.getElementById('cat-2-name').innerHTML = cat.two;
+// Create HTML elements for cats
+var HTMLcatName = '<p class="cat-name">%data%</p>';
+var HTMLcatPic = '<img src="%data%" class="cat-pic img-fluid">';
+var HTMLcatCount = '<p class="cat-score">%data%</p>';
 
+//Display cat two object
+catTwo.display = function() {
 
-// var catNames = document.getElementsByClassName("cat-name");
-// for(var i = 0; i < catNames.length; i++)
-// {
-//   catNames.innterHTML = cat.one;
-//   console.log(cat.one);
-// }
+  var formattedName = HTMLcatName.replace("%data%", catTwo.name);
+  var formattedPic = HTMLcatPic.replace("%data%", catTwo.photo);
+  var formattedCount = HTMLcatCount.replace("%data%", catTwo.counter);
+  $("#cat-2").append(formattedName, formattedPic, formattedCount);
+
+  // Create a button for the click counter.. this doesn't work yet
+  var button = formattedPic;
+  var counter2 = formattedCount;
+  count = 0;
+  button.onclick = function() {
+    count += 1;
+    counter2.innerHTML = "Clicks: " + count;
+  };
+}
+
+//Call cat display function
+catTwo.display();
+
